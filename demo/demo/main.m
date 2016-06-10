0abc0ea01c6f28822dd523dbebdd4b8233ec8ff7
User <lulul@lululs-mac-mini.local>
2016-06-10 15:25:50 +0800

    commit base code

---
+++ demo/demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo
+//
+//  Created by lulul on 16/6/10.
+//  Copyright © 2016年 TJPageController. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
